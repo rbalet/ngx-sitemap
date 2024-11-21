2a0908ed372e6e097dbb2933f4826daa54cac52e
Raphaël Balet <raphael.balet@outlook.com>
2024-11-21 16:10:52 +0100

    perf(dependencies/glob): remove

--- bin/index.ts
+++ bin/index.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-import { SitemapGenerator } from './sitemap-generator';
+import { SitemapGenerator } from "./sitemap-generator";
 
 const sitemapGenerator = new SitemapGenerator(
-    process.argv[2],
-    process.argv[3],
+  process.argv[2],
+  process.argv[3],
+  process.argv[4]
 );
 
 sitemapGenerator.process();
--- bin/sitemap-generator.ts
+++ bin/sitemap-generator.ts
@@ -1,67 +1,103 @@
-import { Glob } from 'glob';
-import * as fs from 'fs';
+import * as fs from "fs";
+import * as path from "path";
 
 export class SitemapGenerator {
-    private readonly SITEMAP_HEADER = `<?xml version="1.0" encoding="UTF-8"?>
+  private readonly SITEMAP_HEADER = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 
-    private readonly SITEMAP_FOOTER = `
+  private readonly SITEMAP_FOOTER = `
 </urlset>`;
 
-    private readonly baseUrl: string;
-    private readonly srcDirectory: string;
+  private readonly baseUrl: string;
+  private readonly srcDirectory: string;
 
-    private sitemapContent = '';
+  private sitemapContent = "";
 
-    constructor(srcDirectory: string, baseUrl: string) {
-        this.baseUrl = SitemapGenerator.withTrailingSlash(baseUrl);
-        this.srcDirectory = SitemapGenerator.withTrailingSlash(srcDirectory);
+  constructor(srcDirectory: string, baseUrl: string) {
+    this.baseUrl = this.withTrailingSlash(baseUrl);
+    this.srcDirectory = this.withTrailingSlash(srcDirectory);
+  }
+
+  private _getToday() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, "0"); // Months are 0-indexed
+    const day = today.getDate().toString().padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  }
+
+  private withTrailingSlash(text: string) {
+    if (!text.endsWith("/")) {
+      return text + "/";
+    }
+    return text;
+  }
+
+  async process(): Promise<void> {
+    try {
+      const files = await this.findFiles(this.srcDirectory, "index.html");
+      this.filesCallback(null, files);
+    } catch (err) {
+      this.filesCallback(err as Error, []);
+    }
+  }
+
+  private async findFiles(dir: string, fileName: string): Promise<string[]> {
+    const results: string[] = [];
+    const entries = await fs.promises.readdir(dir, { withFileTypes: true });
+
+    for (const entry of entries) {
+      const fullPath = path.join(dir, entry.name);
+      if (entry.isDirectory()) {
+        const subDirFiles = await this.findFiles(fullPath, fileName);
+        results.push(...subDirFiles);
+      } else if (entry.isFile() && entry.name === fileName) {
+        results.push(fullPath);
+      }
     }
 
-    private static withTrailingSlash(text: string) {
-        if (!text.endsWith('/')) {
-            return text + '/';
-        }
-        return text;
-    }
+    return results;
+  }
 
-    process(): void {
-        new Glob(this.srcDirectory + '/**/index.html', (err: Error, res: string[]) => this.filesCallback(err, res));
-    }
-
-    private addToSitemap(file: string): void {
-        let url = file.replace(this.srcDirectory, this.baseUrl);
-        url = url.replace('/index.html', '');
-        this.sitemapContent += `
+  private addToSitemap(file: string): void {
+    let url = file.replace(this.srcDirectory, this.baseUrl);
+    url = url.replace("/index.html", "");
+    this.sitemapContent += `
     <url>
-        <loc>${ url }</loc>
+        <loc>${url}</loc>
     </url>`;
-    }
+  }
 
-    private writeSitemapToFile(): void {
-        fs.writeFile(this.srcDirectory + 'sitemap.xml', this.SITEMAP_HEADER + this.sitemapContent + this.SITEMAP_FOOTER, (err: NodeJS.ErrnoException | null) => {
-            if (err) {
-                console.log(err.message);
-                return;
-            }
-
-            console.log(`sitemap.xml successfully created in '${ this.srcDirectory }'`);
-        });
-    }
-
-
-    private filesCallback(err: Error, files: string[]): void {
+  private writeSitemapToFile(): void {
+    fs.writeFile(
+      path.join(this.srcDirectory + "sitemap.xml"),
+      this.SITEMAP_HEADER + this.sitemapContent + this.SITEMAP_FOOTER,
+      (err: NodeJS.ErrnoException | null) => {
         if (err) {
-            console.log('Error', err);
-            return;
+          console.log(err.message);
+          return;
         }
 
-        files.sort((a, b) => {
-            return a.length - b.length || a.localeCompare(b);
-        });
+        console.log(
+          `sitemap.xml successfully created in '${this.srcDirectory}'`
+        );
+      }
+    );
+  }
 
-        files.forEach(route => this.addToSitemap(route));
-
-        this.writeSitemapToFile();
+  private filesCallback(err: Error, files: string[]): void {
+    if (err) {
+      console.log("Error", err);
+      return;
     }
+
+    files.sort((a, b) => {
+      return a.length - b.length || a.localeCompare(b);
+    });
+
+    files.forEach((route) => this.addToSitemap(route));
+
+    this.writeSitemapToFile();
+  }
 }
